a6df8caf0bbc41632d2a068fc20564bcfa67c4e7
just3260@gmail.com <just3260@gmail.com>
2023-02-21 17:50:38 +0800

    update User model

--- Sources/JOJACore/APIModels/SignupAPIModel.swift
+++ Sources/JOJACore/APIModels/SignupAPIModel.swift
@@ -9,10 +9,12 @@ import Foundation
 
 public struct SignupAPIModel: Codable {
     public let username: String
+    public let email: String
     public let password: String
     
-    public init(username: String, password: String) {
+    public init(username: String, email: String, password: String) {
         self.username = username
+        self.email = email
         self.password = password
     }
 }
--- Sources/JOJACore/APIModels/UserAPIModel.swift
+++ Sources/JOJACore/APIModels/UserAPIModel.swift
@@ -10,14 +10,18 @@ import Foundation
 public struct UserAPIModel: Codable {
     public let id: UUID
     public let username: String
+    public let email: String
+    public let isAdmin: Bool
     public let password: String
     public let createdAt: Date
     public let updatedAt: Date
     
-    public init(id: UUID, username: String, password: String, createdAt: Date, updatedAt: Date) {
+    public init(id: UUID, username: String, email: String, password: String, isAdmin: Bool, createdAt: Date, updatedAt: Date) {
         self.id = id
         self.username = username
+        self.email = email
         self.password = password
+        self.isAdmin = isAdmin
         self.createdAt = createdAt
         self.updatedAt = updatedAt
     }
@@ -26,15 +30,18 @@ public struct UserAPIModel: Codable {
 extension UserAPIModel {
     public struct Create: Codable {
         public let username: String
+        public let email: String
         public let password: String
         
-        public init(username: String, password: String) {
+        public init(username: String, email: String, password: String) {
             self.username = username
+            self.email = email
             self.password = password
         }
         
         public init(signup: SignupAPIModel) {
             self.username = signup.username
+            self.email = signup.email
             self.password = signup.password
         }
     }
@@ -44,12 +51,16 @@ extension UserAPIModel {
     public struct Public: Codable {
         public let id: UUID
         public let username: String
+        public let email: String
+        public let isAdmin: Bool
         public let createdAt: Date
         public let updatedAt: Date
         
-        public init(id: UUID, username: String, createdAt: Date, updatedAt: Date) {
+        public init(id: UUID, username: String, email: String, isAdmin: Bool, createdAt: Date, updatedAt: Date) {
             self.id = id
             self.username = username
+            self.email = email
+            self.isAdmin = isAdmin
             self.createdAt = createdAt
             self.updatedAt = updatedAt
         }
