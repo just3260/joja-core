28a3a2eac02b86c6289ccc892e51dbbac3813855
Andrew Wang <just3260@gmail.com>
2025-01-10 02:59:20 +0800

    update

--- Sources/JOJACore/APIModels/ProductAPIModel.swift
+++ Sources/JOJACore/APIModels/ProductAPIModel.swift
@@ -60,22 +60,31 @@ public struct ProductAPIModel: Codable, Hashable {
 extension ProductAPIModel {
     public struct Request: Codable, Hashable {
         
+        public let fabricId: UUID?
         public let brand: TypeAPIModel.Brand
-        public let amount: Int
-        public let count: Int
+        public let goods: TypeAPIModel.Goods
+        public let size: Size
+        public let price: Int
+        public let storage: TypeAPIModel.Location
         public let discount: Int?
         public let note: String?
         
         public init(
+            fabricId: UUID?,
             brand: TypeAPIModel.Brand,
-            amount: Int,
-            count: Int,
+            goods: TypeAPIModel.Goods,
+            size: Size,
+            price: Int,
+            storage: TypeAPIModel.Location,
             discount: Int?,
             note: String?
         ) {
+            self.fabricId = fabricId
             self.brand = brand
-            self.amount = amount
-            self.count = count
+            self.goods = goods
+            self.size = size
+            self.price = price
+            self.storage = storage
             self.discount = discount
             self.note = note
         }
@@ -84,30 +93,43 @@ extension ProductAPIModel {
 
 extension ProductAPIModel {
     public struct Response: Codable, Hashable {
+        
         public let id: UUID
+        public let fabricId: UUID?
         public let brand: TypeAPIModel.Brand
-        public let amount: Int
-        public let count: Int
+        public let goods: TypeAPIModel.Goods
+        public let size: Size
+        public let price: Int
+        public let storage: TypeAPIModel.Location
         public let note: String?
-        public let tradeID: UUID
+        public let tradeID: UUID?
         public let createdAt: Date?
+        public let updatedAt: Date?
         
         public init(
             id: UUID,
+            fabricId: UUID?,
             brand: TypeAPIModel.Brand,
-            amount: Int,
-            count: Int,
+            goods: TypeAPIModel.Goods,
+            size: Size,
+            price: Int,
+            storage: TypeAPIModel.Location,
             note: String?,
-            tradeID: UUID,
-            createdAt: Date?
+            tradeID: UUID?,
+            createdAt: Date?,
+            updatedAt: Date?
         ) {
             self.id = id
+            self.fabricId = fabricId
             self.brand = brand
-            self.amount = amount
-            self.count = count
+            self.goods = goods
+            self.size = size
+            self.price = price
+            self.storage = storage
             self.note = note
             self.tradeID = tradeID
             self.createdAt = createdAt
+            self.updatedAt = updatedAt
         }
     }
 }
