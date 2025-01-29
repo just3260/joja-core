4104d75704eb5d7b4c737d270dc00bf5b78cf5ad
Andrew Wang <just3260@gmail.com>
2025-01-29 20:05:32 +0800

    update

--- Sources/JOJACore/APIModels/SearchAPIModel.swift
+++ Sources/JOJACore/APIModels/SearchAPIModel.swift
@@ -10,7 +10,7 @@ import Foundation
 public protocol Searchable: Codable, Hashable {}
 
 public struct SearchType: Codable, Hashable {
-    /// 搜尋類型
+    /// Member 搜尋類型
     public enum Member: String, Searchable, CaseIterable {
         case name // 姓名
         case phone // 電話
@@ -56,7 +56,7 @@ public struct SearchType: Codable, Hashable {
         }
     }
     
-    /// 搜尋類型
+    /// Fabric 搜尋類型
     public enum Fabric: String, Searchable, CaseIterable {
         case name // 名稱
         case material // 材質
@@ -101,6 +101,73 @@ public struct SearchType: Codable, Hashable {
                 return "建立日期"
             case .updatedAt:
                 return "最後更新日期"
+            }
+        }
+    }
+    
+    /// Product 搜尋類型
+    public enum Product: String, Searchable, CaseIterable {
+        case name // 名稱
+        case brand // 品牌
+        case type // 商品類型
+        case jojaFabric // JOJA 布料商品
+        case jojaOther // JOJA 其他商品
+        case otherGoods // 其他品牌商品
+        case size // 尺寸
+        case material // 材質
+        case cottonMaterial // 棉布材質
+        case age // 布料年代
+        case design // 花色
+        case color // 顏色
+        case sn // 商品編號
+        case storage // 存放位置
+        case tag // tag
+        case note // 備註
+        case createdAt // 建立日期
+        case updatedAt // 最後更新日期
+        
+        public static func getKey() -> String {
+            "product_search_type"
+        }
+        
+        public func getName() -> String {
+            switch self {
+                case .name:
+                    return "名稱"
+                case .brand:
+                    return "品牌"
+                case .type:
+                    return "商品類型"
+                case .jojaFabric:
+                    return "JOJA 布料商品"
+                case .jojaOther:
+                    return "JOJA 其他商品"
+                case .otherGoods:
+                    return "其他品牌商品"
+                case .size:
+                    return "尺寸"
+                case .material:
+                    return "材質"
+                case .cottonMaterial:
+                    return "棉布材質"
+                case .age:
+                    return "布料年代"
+                case .design:
+                    return "花色"
+                case .color:
+                    return "顏色"
+                case .sn:
+                    return "布料編號"
+                case .storage:
+                    return "存放位置"
+                case .tag:
+                    return "tag"
+                case .note:
+                    return "備註"
+                case .createdAt:
+                    return "建立日期"
+                case .updatedAt:
+                    return "最後更新日期"
             }
         }
     }
