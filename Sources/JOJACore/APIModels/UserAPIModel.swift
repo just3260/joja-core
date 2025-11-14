6ab1154d49c57ae1d7eda9bc6a773f313e69aaa9
just3260 <just3260@gmail.com>
2025-11-14 21:18:15 +0800

    update

--- Sources/JOJACore/APIModels/UserAPIModel.swift
+++ Sources/JOJACore/APIModels/UserAPIModel.swift
@@ -28,24 +28,51 @@ public struct PermissionOptions: OptionSet, Codable, Hashable, Sendable {
     public static let deleteTag = PermissionOptions(rawValue: 1 << 10) // 1024
 }
 
-public struct StatisticsAPIModel: Codable, Hashable, Sendable {
-    public let candidateCount: Int
+public struct RevenueAPIModel: Codable, Hashable, Sendable {
+    public let location: TypeAPIModel.SpendingLocation
     public let todayTransactions: Int
     public let todayRevenue: Int
+    public let monthTransactions: Int
     public let monthRevenue: Int
-    
-    public init(candidateCount: Int = 0, todayTransactions: Int = 0, todayRevenue: Int = 0, monthRevenue: Int = 0) {
-        self.candidateCount = candidateCount
+
+    public init(
+        location: TypeAPIModel.SpendingLocation,
+        todayTransactions: Int,
+        todayRevenue: Int,
+        monthTransactions: Int,
+        monthRevenue: Int
+    ) {
+        self.location = location
         self.todayTransactions = todayTransactions
         self.todayRevenue = todayRevenue
+        self.monthTransactions = monthTransactions
         self.monthRevenue = monthRevenue
+    }
+
+    public static let sample: RevenueAPIModel = .init(
+        location: .tainan,
+        todayTransactions: 5,
+        todayRevenue: 5000,
+        monthTransactions: 10,
+        monthRevenue: 10000
+    )
+}
+
+public struct StatisticsAPIModel: Codable, Hashable, Sendable {
+    public let candidateCount: Int
+    public let revenues: [RevenueAPIModel]
+
+    public init(
+        candidateCount: Int = 0,
+        revenues: [RevenueAPIModel]
+    ) {
+        self.candidateCount = candidateCount
+        self.revenues = revenues
     }
     
     public static let sample: StatisticsAPIModel = .init(
         candidateCount: 3,
-        todayTransactions: 12,
-        todayRevenue: 16800,
-        monthRevenue: 350000
+        revenues: [RevenueAPIModel.sample]
     )
 }
 
